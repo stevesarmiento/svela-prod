ab59eee08d79141a3c7ee87d4980e2e7a8567f8a
Steven <sarmiento.steven@gmail.com>
2025-07-10 13:45:48 -0700

    fix build

--- apps/app/next.config.ts
+++ apps/app/next.config.ts
@@ -8,7 +8,7 @@ const nextConfig = {
   reactStrictMode: true,
   transpilePackages: [],
   experimental: {
-    optimizeCss: isProd,
+    //optimizeCss: isProd,
     optimizePackageImports: isProd ? ["@v1/ui"] : [],
     serverActions: {
       bodySizeLimit: "2mb",
