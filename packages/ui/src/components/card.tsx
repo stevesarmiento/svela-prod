64aa93de0effe7dacc04331d08d9a58b8b23f7cd
Steven <sarmiento.steven@gmail.com>
2026-07-14 01:23:48 -0700

    Update card.tsx

--- packages/ui/src/components/card.tsx
+++ packages/ui/src/components/card.tsx
@@ -56,7 +56,7 @@ const CardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn( className)} {...props} />
+  <div ref={ref} className={cn(className)} {...props} />
 ));
 CardContent.displayName = "CardContent";
 
